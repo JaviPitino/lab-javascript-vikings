95dabbbe19926187b6e89b00ca2c2fa21dddd7eb
Javier López <javierpitino@gmail.com>
2022-04-18 21:07:56 +0200

    resultados. Bonus;por la mitad :(

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,89 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+  constructor(healthParam, strengthParam ) {
+    this.health = healthParam;
+    this.strength = strengthParam;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damageParam) {
+    this.health -= damageParam;
+  }
+};
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor( nameParam, healthParam, strengthParam ) {
+    super(healthParam, strengthParam)
+    this.name = nameParam;
+  }
+    receiveDamage(damageParam) {
+      this.health -= damageParam;
+      if ( this.health > 0 ) {
+        return `${this.name} has received ${damageParam} points of damage`;
+      } else {
+        return `${this.name} has died in act of combat`;
+      }
+    }
+    battleCry() {
+      return `Odin Owns You All!`;
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier  {
+ 
+  receiveDamage(damageParam) {
+    this.health -= damageParam;
+    if ( this.health > 0 ) {
+      return `A Saxon has received ${damageParam} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+
+}
 
 // War
-class War {}
+class War extends Soldier {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  };
+  addViking(vikingObj) {
+    this.vikingArmy.push(vikingObj);
+  }
+  addSaxon(saxonObj) {
+    this.saxonArmy.push(saxonObj);
+  };
+  vikingAttack() {
+    // Convertimos en numeros enteros y randoms cada soldado
+    let vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+    let saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+
+    // lucha, soldado random pierde salud(health) por fuerza (strength)
+    let fight = saxonRandom.receiveDamage(vikingRandom.strength);
+    
+    // Condición para eliminar Saxon
+    if ( saxonRandom.health <= 0 ) {
+      saxonArmy.splice(saxonRandomWar, 1)
+    };
+  
+  return fight;
+
+  }
+
+
+  receiveDamage(damageParam) {
+      this.health -= damageParam;
+  }
+  saxonAttack() {
+
+  }
+  showStatus() {
+
+  }
+}
 
 
 
